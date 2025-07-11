3a3867e797f450cf51618b9971d568442833329a
Mariana <mail.marianamendes@gmail.com>
2025-07-11 12:50:33 +0100

    Remove storyboard

---
+++ StarWars/StarWars/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Mariana Mendes on 11/07/2025.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
